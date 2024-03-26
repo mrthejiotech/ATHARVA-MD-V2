a718e5150cf364f60764c41738bb21dba7f1a6c6
botskaking <152233761+botskaking@users.noreply.github.com>
2024-03-26 10:01:52 +0530

    Update econ-slot.js

--- plugins/econ-slot.js
+++ plugins/econ-slot.js
@@ -18,11 +18,11 @@ ${usedPrefix + command} 500*`.trim()
     let users = global.db.data.users[m.sender]
     let time = users.lastslot + 10000
     if (new Date - users.lastslot < 10000) throw `⏳ Wait *${msToTime(time - new Date())}* to use again`
-    if (amount < 500) throw `🟥 *You can't bet gold less than 500*`
+    if (amount < 1100) throw `🟥 *You can't bet gold less than 100*`
     if (users.credit < amount) {
         throw `🟥 *You do not have enough gold to bet*`
     }
-    if (amount > 100000) throw `🟥 *You can't bet gold more than 100000*`
+    if (amount > 1000000) throw `🟥 *You can't bet gold more than 100000*`
 
     let emojis = ["🕊️", "🦀", "🦎"];
     let a = Math.floor(Math.random() * emojis.length);
@@ -70,7 +70,7 @@ ${end}`)
 }
 handler.help = ['slot <amount>']
 handler.tags = ['game']
-handler.command = ['slot']
+handler.command = ['slot','bet']
 
 handler.group = true
 
