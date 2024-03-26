136c014de6befdaf56c31e3583e842370ccf040f
botskaking <152233761+botskaking@users.noreply.github.com>
2024-03-26 10:04:37 +0530

    Update econ-daily.js

--- plugins/econ-daily.js
+++ plugins/econ-daily.js
@@ -1,5 +1,5 @@
-const free = 2000
-const prem = 5000
+const free = 10000
+const prem = 5000000
 
 let handler = async (m, {conn, isPrems }) => {
   let time = global.db.data.users[m.sender].lastclaim + 86400000
