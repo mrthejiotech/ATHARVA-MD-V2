3d2e1ce9c176665d6fef0c520660735aeea9d6f7
botskaking <152233761+botskaking@users.noreply.github.com>
2024-04-10 11:04:17 +0530

    Create main-hi.js

---
+++ plugins/main-hi.js
@@ -0,0 +1,41 @@
+
+
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+
+    // Sound
+    let name = m.pushName || conn.getName(m.sender)
+    var vn = "./Assets/ALIVE.mp3"
+    let url = "https://github.com/botskaking"
+    let murl = "https://YouTube.com"
+    let img = "https://iili.io/JkiDQwu.jpg"
+    let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
+    let doc = {
+        audio: {
+          url: vn
+        },
+        mimetype: 'audio/mp4',
+        ptt: true,
+        waveform:  [100, 0, 100, 0, 100, 0, 100],
+        fileName: "Guru",
+    
+        contextInfo: {
+          mentionedJid: [m.sender],
+          externalAdReply: {
+          title: "hi",
+          body: "Hello! Dear User",
+          thumbnailUrl: img,
+          sourceUrl: 'https://chat.whatsapp.com/EmP3syvou18HrZk6R6nTAK',
+          mediaType: 1,
+          renderLargerThumbnail: false
+          }}
+      };
+    
+      await conn.sendMessage(m.chat, doc, { quoted: con });
+    
+    }
+    
+    handler.help = ['alive']
+    handler.tags = ['main']
+    handler.command = /^(hi)$/i 
+
+    export default handler;
