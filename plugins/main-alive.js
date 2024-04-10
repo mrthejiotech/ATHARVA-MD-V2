a3bb68516ac55397aff62d83a39e60b6d173a205
botskaking <152233761+botskaking@users.noreply.github.com>
2024-04-10 11:01:39 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -5,9 +5,9 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     // Sound
     let name = m.pushName || conn.getName(m.sender)
     var vn = "./Assets/ALIVE.mp3"
-    let url = "https://github.com/Xcelsama"
+    let url = "https://github.com/botskaking"
     let murl = "https://YouTube.com"
-    let img = "https://telegra.ph/file/f2fa9732e0e26bbb0bfb0.jpg"
+    let img = "https://iili.io/JkiDQwu.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
