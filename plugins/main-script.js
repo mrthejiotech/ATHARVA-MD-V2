1c1161716fd9e6fbf4b5dcbd5d4e1f46f2904700
botskaking <152233761+botskaking@users.noreply.github.com>
2024-04-10 10:50:59 +0530

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -3,7 +3,7 @@ import { join } from 'path';
 import axios from 'axios'; 
 
 let handler = async function (m, { conn, __dirname }) {
-  const githubRepoURL = 'https://github.com/Xcelsama/STAR-MD-V2';
+  const githubRepoURL = 'https://github.com/botskaking/ATHARVA-MD-V2/';
 
   try {
   
@@ -16,18 +16,14 @@ let handler = async function (m, { conn, __dirname }) {
 
       // Format the repository information with emojis
       const formattedInfo = `
-    ─────《 𝐒𝐓𝐀𝐑-𝐌𝐃-𝐕𝟐 》─────⊷
- 📂 *ᏴϴͲ ΝᎪᎷᎬ:* _${repoData.name}_
-📝 *ᎠᎬՏᏟᎡᏆᏢͲᏆϴΝ:* _Ꭺ ᏔᎻᎪͲՏᎪᏢᏢ ᏴϴͲ ᎡᎬᏟϴᎠᎬᎠ ᏴᎽ ᎬХᏟᎬᏞ... ᏔᏆͲᎻ ᏞϴͲՏ ϴҒ ᎡᏆᏟᎻ ҒႮΝᏟͲᏆϴΝՏ.... 
-👤 *ᎠᎬᏙᎬᏞϴᏢᎬᎡ:* _EXCEL AMADI_
-⭐ *ՏͲᎪᎡՏ:* _${repoData.stargazers_count}_ 
-🍴 *ҒϴᎡᏦՏ:* _${repoData.forks_count}_   
-🔍 *ᎦᏟᎪΝ-ϘᎡ:*https://session.guruapi.tech/
-⚔️ *ᘜᖇOᑌᑭ:* https://chat.whatsapp.com/Lg0lY4M1k8oDMYzylg86xs
-📡 *ᑭᑌᗷᒪIᑕ ᏀᖇOᑌᑭ:* https://chat.whatsapp.com/EmP3syvou18HrZk6R6nTAK
-💻 *ᑕᕼᗩᑎᑎᗴᒪ ᒪIᑎK:* https://whatsapp.com/channel/0029Va9wmuz8F2pGIURwmo0m 
-🌐 *ႮᎡᏞ:* ${repoData.html_url}
-🌠 *ΝϴᏔ ᎠᎬᏢᏞϴᎽ*:-https://dashboard.heroku.com/new?template=https://github.com/Xcelsama/STAR-MD-V2' 
+    ─────《 ATHARVA-MD-V2 》─────⊷
+* 📂 *ᏴϴͲ ΝᎪᎷᎬ:* _${repoData.name}_
+* 📝 *ᎠᎬՏᏟᎡᏆᏢͲᏆϴΝ:* _Ꭺ ᏔᎻᎪͲՏᎪᏢᏢ ᏴϴͲ ᎡᎬᏟϴᎠᎬᎠ ᏴᎽ IRONMAN AND ATHARVA... ᏔᏆͲᎻ ᏞϴͲՏ ϴҒ ᎡᏆᏟᎻ ҒႮΝᏟͲᏆϴΝՏ.... 
+* 👤 *ᎠᎬᏙᎬᏞϴᏢᎬᎡ:* _IRONMAN AND ATHARVA_
+* ⭐ *ՏͲᎪᎡՏ:* _${repoData.stargazers_count}_ 
+* 🍴 *ҒϴᎡᏦՏ:* _${repoData.forks_count}_   
+* 🌐 *ႮᎡᏞ:* ${repoData.html_url}
+* 🌠 *ΝϴᏔ ᎠᎬᏢᏞϴᎽ*:-https://dashboard.heroku.com/new?template=https://github.com/botskaking/ATHARVA-MD-V2' 
 
  `.trim();
 
